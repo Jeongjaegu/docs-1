6486800c8099822dc1b7dcd38f4725e3b3ba50e7
Fay Montage <accounts@faymontage.com>
2016-12-13 12:55:32 +0100

    Specify user/email for git

--- deploy-blueprint.sh
+++ deploy-blueprint.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")"
 
 mkdir -p $1
 aglio --theme-full-width -i "$SRC_DIR/$2" -o "$1/index.html"
+
+git config --global user.name "CircleCI"
+git config --global user.email "root@circleci.com"
 git add "$1/index.html"
 git commit -m "Update docs for $1 commit $3"
 git push origin master
